32c1644ca3755a4753c3e1f65ce48e547408d817
SAISSIOMAR <omar.saissi@etu.univ-cotedazur.fr>
2022-12-03 21:37:30 +0100

    active mq

--- WebRoutingServer/BikingApp/Program.cs
+++ WebRoutingServer/BikingApp/Program.cs
@@ -7,6 +7,9 @@ using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 using WebRoutingServer;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
 
 namespace BikingApp
 {
@@ -58,7 +61,55 @@ namespace BikingApp
             {
                 Console.WriteLine(s.instruction);
             }
-           
+            // Create a Connection Factory.
+            Uri connecturi = new Uri("activemq:tcp://localhost:61616");
+            ConnectionFactory connectionFactory = new ConnectionFactory(connecturi);
+
+            // Create a single Connection from the Connection Factory.
+            IConnection connection = connectionFactory.CreateConnection();
+            connection.Start();
+
+            // Create a session from the Connection.
+            ISession session = connection.CreateSession();
+
+            // Use the session to target a queue.
+            IDestination destination = session.GetQueue("test");
+
+            // Create a Producer targetting the selected queue.
+            IMessageProducer producer = session.CreateProducer(destination);
+
+            // You may configure everything to your needs, for instance:
+
+
+            // Finally, to send messages:
+            string message1 = "";
+            foreach (Step s in s1)
+            {
+                message1 = message1 + "\n " + s.instruction.ToString();
+            }
+
+            Console.WriteLine(message1);
+
+            ITextMessage message = session.CreateTextMessage(message1);
+            producer.Send(message);
+
+
+
+
+
+
+
+
+            Console.WriteLine("Message sent, check ActiveMQ web interface to confirm.");
+            Console.ReadLine();
+
+            // Don't forget to close your session and connection when finished.
+            session.Close();
+            connection.Close();
+
+
+            Console.ReadLine();
+
 
 
 
